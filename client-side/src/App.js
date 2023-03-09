c72531b95df233e580655b20fd241282eee14108
OmerBensadon <omerben1989@gmail.com>
2023-03-09 14:11:26 +0200

    OB - Login
    9.3.23 First login form

---
+++ client-side/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import LoginForm from "./Components/LoginForm";
+function App() {
+  return (
+    <div className="page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client-side/src/Components/LoginForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <>
+      <div className="cover">
+        <img src=""></img>
+
+        <label>כתובת מייל</label>
+        <input placeholder="Anonimus@gmail.com" type="text"></input>
+        <label>סיסמא</label>
+        <input placeholder="******" type="text"></input>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
